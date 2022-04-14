dbe70bd5c6e6af8bd486397a3106ee1ede8ef188
anhtay <tayphongtran@gmail.com>
2022-04-14 16:22:59 +0700

    up

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int n, i, c;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("Nhập vào số phần tử của mảng: ");
+            n = scanner.nextInt();
+        } while (n <= 0);
+
+        int A[] = new int[n];
+
+        System.out.println("Nhập các phần tử cho mảng: ");
+        for (i = 0; i < n; i++) {
+            System.out.print("Nhập phần tử thứ " + i + ": ");
+            A[i] = scanner.nextInt();
+        }
+
+        System.out.println("Nhập số nguyên k: ");
+        int k = scanner.nextInt();
+
+        for (c = i = 0; i < n; i++) {
+            if (A[i] != k) {
+                A[c] = A[i];
+                c++;
+            }
+        }
+
+        n = c;
+        System.out.println("Mảng còn lại sau khi xóa phần tử " + k + " là: ");
+        for (i = 0; i < n; i++) {
+            System.out.print(A[i] + "\t");
+        }
+
+    }
+}
